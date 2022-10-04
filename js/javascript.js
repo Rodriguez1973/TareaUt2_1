a901eb438a3748f2fc7ef938bfedb4914485c028
Rodriguez <j062667@gmail.com>
2022-10-04 10:13:21 +0200

    Se añade enunciado de la tarea.

--- js/javascript.js
+++ js/javascript.js
@@ -1,56 +1,77 @@
 /*Captura el evento de cambio en la <select id="operacion">*/
-document.getElementById('operacion').onchange = function () {
+//document.getElementById('operacion').onchange = function () {
+//  realizarConversion();
+//}
+operacion.onchange = function () {
   realizarConversion();
 }
 
+/*Captura el evento de cambio en la <input id="numero">*/
+//document.getElementById('numero').onchange = function () {
+//  realizarConversion();
+//}
+numero.onchange = function () {
+  realizarConversion();
+}
+
+
 /*Captura el evento de onclick en la <p id="cerrar_navegador">*/
-document.getElementById("cerrar_navegador").onclick = function () {
+// document.getElementById("cerrar_navegador").onclick = function () {
+//   window.close();
+// }
+cerrar_navegador.onclick = function () {
   window.close();
 }
 
 /*Función que realiza la conversión del tipo de dato*/
 function realizarConversion() {
-  let numero = document.getElementById('numero').value;
-  let resultado = "";
-  //Existe contenido en el input con id="numero".
-  if (numero) {
+  //let num = document.getElementById('numero').value;
+  let num = numero.value;
+  let resul = "";
+  //Existe contenido en el input con id="num".
+  if (num) {
     //Si se puede convertir a un número.
-    if (!isNaN(numero)) {
-      let opcion = document.getElementById('operacion').value;
+    if (!isNaN(num)) {
+      //let opcion = document.getElementById('operacion').value;
+      let opcion = operacion.value;
       if (opcion != "no_convertir") {
-        resultado = cambiaTipo(numero, opcion);
+        resul = cambiaTipo(num, opcion);
       }
     } else {
-      resultado = 'La conversión no es posible';
+      resul = 'La conversión no es posible';
     }
-    document.getElementById('resultado').value = resultado;
+    //document.getElementById('resultado').value = resul;
+    resultado.value = resul;
   }
 }
 
 //Función que devuelve el valor convertido.
-function cambiaTipo(numero, opcion) {
-  let res = NaN
+function cambiaTipo(num, opcion) {
+  let result = 'La conversión no es posible';
   switch (opcion) {
     case 'dec_bin':
-      res = parseInt(numero).toString(2);
+      result = parseInt(num).toString(2);
       break
     case 'dec_oct':
-      res = parseInt(numero).toString(8);
+      result = parseInt(num).toString(8);
       break
     case 'dec_hex':
-      res = parseInt(numero).toString(16);
+      result = parseInt(num).toString(16);
       break
     case 'bin_dec':
-      res = parseInt(numero, 2);
+      result = parseInt(num, 2);
       break
     case 'oct_dec':
-      res = parseInt(numero, 8);
+      result = parseInt(num, 8);
       break
     case 'hex_dec':
-      res = parseInt(numero, 16);
+      result = parseInt(num, 16);
       break
     default:
       break
   }
-  return res;
+  return result;
 }
+
+/*Función que realiza la validación de la cadena antes de la conversión.*/
+function validarCadena()
