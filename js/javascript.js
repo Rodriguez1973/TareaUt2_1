e3271a3e6b96de7c209945d37b3782971311649b
Rodriguez1973 <j062667@gmail.com>
2022-10-04 18:55:02 +0200

    Se codifica la función para validar la cadena

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@
 //  realizarConversion();
 //}
 operacion.onchange = function () {
-  realizarConversion();
+  realizarConversion()
 }
 
 /*Captura el evento de cambio en la <input id="numero">*/
@@ -11,67 +11,110 @@ operacion.onchange = function () {
 //  realizarConversion();
 //}
 numero.onchange = function () {
-  realizarConversion();
+  realizarConversion()
 }
-
 
 /*Captura el evento de onclick en la <p id="cerrar_navegador">*/
 // document.getElementById("cerrar_navegador").onclick = function () {
 //   window.close();
 // }
 cerrar_navegador.onclick = function () {
-  window.close();
+  window.close()
 }
 
 /*Función que realiza la conversión del tipo de dato*/
 function realizarConversion() {
   //let num = document.getElementById('numero').value;
-  let num = numero.value;
-  let resul = "";
+  let num = numero.value.toUpperCase()
+  let result = ''
   //Existe contenido en el input con id="num".
   if (num) {
+    //let opcion = document.getElementById('operacion').value;
+    let opcion = operacion.value
     //Si se puede convertir a un número.
-    if (!isNaN(num)) {
-      //let opcion = document.getElementById('operacion').value;
-      let opcion = operacion.value;
-      if (opcion != "no_convertir") {
-        resul = cambiaTipo(num, opcion);
+    if (validarCadena(num, opcion)) {
+      if (opcion != 'no_convertir') {
+        result = cambiaTipo(num, opcion)
       }
     } else {
-      resul = 'La conversión no es posible';
+      result = 'La cadena no se puede convertir.'
     }
-    //document.getElementById('resultado').value = resul;
-    resultado.value = resul;
+    //document.getElementById('resultado').value = result;
+    resultado.value = result
   }
 }
 
 //Función que devuelve el valor convertido.
 function cambiaTipo(num, opcion) {
-  let result = 'La conversión no es posible';
+  let result = ''
   switch (opcion) {
     case 'dec_bin':
-      result = parseInt(num).toString(2);
+      result = parseInt(num).toString(2)
       break
     case 'dec_oct':
-      result = parseInt(num).toString(8);
+      result = parseInt(num).toString(8)
       break
     case 'dec_hex':
-      result = parseInt(num).toString(16);
+      result = parseInt(num).toString(16)
       break
     case 'bin_dec':
-      result = parseInt(num, 2);
+      result = parseInt(num, 2)
       break
     case 'oct_dec':
-      result = parseInt(num, 8);
+      result = parseInt(num, 8)
       break
     case 'hex_dec':
-      result = parseInt(num, 16);
+      result = parseInt(num, 16)
       break
     default:
       break
   }
-  return result;
+  return result
 }
 
 /*Función que realiza la validación de la cadena antes de la conversión.*/
-function validarCadena()
+function validarCadena(num, opcion) {
+  let valido = true
+  switch (opcion) {
+    //Opciones donde es necesario validar si es una cadena válida para un decimal.
+    case 'dec_bin':
+    case 'dec_oct':
+    case 'dec_hex':
+      for (let i = 0; i < num.length; i++) {
+        if (num.chatAt(i) < '0' || num.chatAt(i) > '9') {
+          valido = false
+          break
+        }
+      }
+      break
+    //Opcion donde es necesario validar si es una cadena válida para un binario.
+    case 'bin_dec':
+      for (let i = 0; i < num.length; i++) {
+        if (num.chatAt(i) < '0' || num.chatAt(i) > '1') {
+          valido = false
+          break
+        }
+      }
+      break
+    //Opcion donde es necesario validar si es una cadena válida para un octal.
+    case 'oct_dec':
+      for (let i = 0; i < num.length; i++) {
+        if (num.chatAt(i) < '0' || num.chatAt(i) > '7') {
+          valido = false
+          break
+        }
+      }
+      break
+    //Opcion donde es necesario validar si es una cadena válida para un hexadecimaloctal.
+    case 'hex_dec':
+      for (let i = 0; i < num.length; i++) {
+        if (!((num.chatAt(i) >= '0' && num.chatAt(i) <= '9') ||(num.chatAt(i) >= 'A' && num.chatAt(i) <= 'Z'))) {
+          valido = false
+          break
+        }
+      }
+      break
+    default:
+      break
+  }
+}
